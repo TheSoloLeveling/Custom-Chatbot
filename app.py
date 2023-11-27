44507c08216dfb56d955fabb642dd23974a65d43
Bouziane Yassine <bouzianeyassine112@gmail.com>
2023-11-27 17:27:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("hello")
+import streamlit as st
+from dotenv import load_dotenv
+
+
+def main():
+
+    load_dotenv()
+    st.set_page_config(page_title="Chatbot for Asthma",
+                       page_icon=":activity:")
+    st.header("Chatbot for Asthma")
+    #st.write(css, unsafe_allow_html=True)
+
+    user_question = st.text_input("Ask anything about asthma:")
+    if user_question:
+        print("test")
+        #handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Base Knowldge")
+        pdf_docs = st.file_uploader(
+            "Upload PDF data", accept_multiple_files=True)
+        st.button("Process")
+
+
+if __name__ == '__main__':
+    main()
