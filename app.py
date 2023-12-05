25f386aaa42b528c350901d3ee6ee4010773c87c
Bouziane Yassine <bouzianeyassine112@gmail.com>
2023-12-05 15:06:20 +0100

    writer

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
+from htmlTemplates import css, bot_template, user_template, js
 from langchain.llms import HuggingFaceHub
+import time
+from streamlit.components.v1 import html
 
 #def get_images
 #def get_file_text
@@ -65,38 +67,71 @@ def get_conversation_chain(vectorstore):
 
     return conversation_chain
 
+def typewriter(text: str, speed: int):
+    tokens = text.split()
+    container = st.empty()
+    for index in range(len(tokens) + 1):
+        curr_full_text = " ".join(tokens[:index])
+        container.markdown(curr_full_text)
+        time.sleep(1 / speed)
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    
+    with st.session_state.container1:
+        
+        
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                #typewriter(text=user_template.replace("{{MSG}}", message.content), speed=10)
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True) 
+        
+        response = st.session_state.conversation({'question': user_question})
+        print(response)
+        st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+        st.write(user_template.replace(
+                    "{{MSG}}", user_question), unsafe_allow_html=True)
+        st.write(bot_template.replace(
+                    "{{MSG}}", response), unsafe_allow_html=True)
+
+
 
 
 def main():
 
     load_dotenv()
+    
     st.set_page_config(page_title="Chatbot for Asthma",
                        page_icon=":activity:")
     st.header("Chatbot for Asthma")
     st.write(css, unsafe_allow_html=True)
 
-
+    
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
+    if "container1" not in st.session_state:
+        st.session_state.container1 = None
+  
+    
+    
+    st.session_state.container1 = st.container()
+    
+    with st.session_state.container1:
+        st.write(bot_template.replace(
+                    "{{MSG}}", "ask anything about asthma ask anything about asthma ask anything about asthma ask anything about asthma"), unsafe_allow_html=True)
+    
+    html(js)
+    user_question =  st.text_input("")
 
-
-    user_question = st.text_input("Ask anything about asthma:")
     if user_question:
         handle_userinput(user_question)
+            
 
     with st.sidebar:
         st.subheader("Base Knowldge")
@@ -113,7 +148,7 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
 
                 # craete vector store, vector database containing embedding of each chunk of text
-                vectorstore = get_vectorstore_instructor(text_chunks)
+                vectorstore = get_vectorstore_openai(text_chunks)
 
                 # create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,3 +1,14 @@
+js = '''
+<script>
+var i = 0;
+var txt = document.getElementsByClassName('typewriter')[0].innerText;
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+
+alert("test");
+</script>
+'''
+
 css = '''
 <style>
 .chat-message {
@@ -23,19 +34,54 @@ css = '''
   padding: 0 1.5rem;
   color: #fff;
 }
+.stTextInput>input {
+    color: #ffffff; 
+}
+.stTextInput {
+    color: #4F8BF9;
+    position: fixed;
+    bottom: 3rem;
+    padding-right: 100px
+}
+
+.typewriter p {
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: .05em solid orange; /* The typewriter cursor */
+  white-space: pre-wrap; /* Keeps the content on a single line */
+ /* Gives that scrolling effect as the typing happens */
+  letter-spacing: .05em; /* Adjust as needed */
+  font-size: 0.9em; /* Adjusts the size of the text */
+  width: 100%; /* Sets the width of the container */
+  text-overflow: ellipsis;
+  animation: 
+    typing 2.0s steps(40, end), /* Adjusts the speed of typing */
+    blink-caret .75s step-end infinite;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
+}
 '''
 
 bot_template = '''
-<div class="chat-message bot">
+<div class="chat-message bot typewriter">
     <div class="avatar">
         <img src="https://d2cbg94ubxgsnp.cloudfront.net/Pictures/2000x1125/9/9/3/512993_shutterstock_715962319converted_920340.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
-    <div class="message">{{MSG}}</div>
+    <div class="message"><p>{{MSG}}</p></div>
 </div>
 '''
 
 user_template = '''
-<div class="chat-message user">
+<div class="chat-message user ">
     <div class="avatar">
         <img src="https://t4.ftcdn.net/jpg/02/29/75/83/240_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg">
     </div>    
